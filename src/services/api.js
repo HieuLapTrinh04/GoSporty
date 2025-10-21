7d4da009c87d889fd3c0c78cfa39a31908ffac59
HieuLapTrinh04 <buihieu11012004@gmail.com>
2025-10-21 12:41:13 +0700

    update

--- src/services/api.js
+++ src/services/api.js
@@ -8,10 +8,11 @@ const api = axios.create({
 });
 
 export const setAuthToken = (token) => {
-  if (token)
+  if (token) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  else
+  } else {
     delete api.defaults.headers.common['Authorization'];
+  }
 };
 
 export default api;
